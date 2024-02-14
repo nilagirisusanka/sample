d8da292c24d9c520500cfaf121da51144211c5fd
nilairisusanka <nilagirisusanka@gmail.com>
2024-02-14 17:50:56 +0530

    commit a change

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-    @GetMapping("/hello")
+    @GetMapping("/helloeveryone")
     public String helloeveryone(){
         return "Hello everyone, Welcome to Spring Boot and happy learning";
     }
--- src/main/java/org/example/sample/newfile.java
+++
@@ -1,4 +0,0 @@
-package org.example.sample;
-
-public class newfile {
-}

cd01b02a0ab2af0ba28df03645f772b7edb5efbd
nilairisusanka <nilagirisusanka@gmail.com>
2024-02-14 15:53:32 +0530

    commit a change

---
+++ src/main/java/org/example/sample/SampleApplication.java
@@ -0,0 +1,15 @@
+package org.example.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//main class
+
+@SpringBootApplication
+public class SampleApplication {
+
+    public static void main(String[] args) {
+//main method
+        SpringApplication.run(SampleApplication.class, args);
+    }
+
+}

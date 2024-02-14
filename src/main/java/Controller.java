e54af99ff6b445628ee6311ab143fa1f4d9fddd2
nilairisusanka <nilagirisusanka@gmail.com>
2024-02-14 19:32:59 +0530

    added a comment in controller

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
     @GetMapping("/helloeveryone")
+    //the end point is helloeveryone
     public String helloeveryone(){
         return "hello everyone, welcome to spring boot and happy learning!";
     }
